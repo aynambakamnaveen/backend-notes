43b301154b61aace1192a677477178a33d616978
aynambakamnaveen <aynambakamnaveen@gmail.com>
2026-04-21 11:19:29 +0530

    added env element in middleware

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-const jwt_secrete = '1d3df0ab22e3d33d821aec03f885da81b80c724c2f8a1e1a78d22e98fcc4d382'
 const protect = (req, res, next) => {
   const token = req.cookies?.token;
 
@@ -8,7 +7,7 @@ const protect = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, jwt_secrete);
+    const decoded = jwt.verify(token, process.env.JWT_SECRETE);
 
     if (!decoded?.id) {
       return res.status(401).json({ message: "Invalid token" });
